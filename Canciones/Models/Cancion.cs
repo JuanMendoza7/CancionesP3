107cd4e601c96d49892b7a8f1e1469266e2823fa
Juan Mendoza <a2020111209@estudiantes.upsa.edu.bo>
2021-11-08 03:11:23 -0400

    Agregar archivos de proyecto.

---
+++ Canciones/Models/Cancion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Canciones.Models
+{
+    public class Cancion
+    {
+        [Key]
+        public string Nombre { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} es requerido")]
+        public string Autor { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} es requerido")]
+        [StringLength(200, MinimumLength = 5, ErrorMessage = "La longitud de {0} debe estar entre {2} y {1}")]
+        public string Animo { get; set; }
+        [Url]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "La longitud de {0} debe estar entre {2} y {1}")]
+        public string Enlace { get; set; }
+
+    }
+}
